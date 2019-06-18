8a7a3a1d54fe780994a479b1c7ede3314c0c9076
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-06-18 00:00:59 -0500

    add attrs to sanitizer

--- db/migrate/20190618043933_devise_create_users.rb
+++ db/migrate/20190618043933_devise_create_users.rb
@@ -34,7 +34,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
 
       t.string :first_name
       t.string :last_name
-      t.string :username
+      t.string :username, unique: true
 
       t.timestamps null: false
     end
