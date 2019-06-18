8a40336c75c6955459f39b3b4524e87622abf217
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-06-18 12:46:44 -0500

    use httparty instead net/http

--- app/controllers/workspace/links_controller.rb
+++ app/controllers/workspace/links_controller.rb
@@ -8,7 +8,7 @@ module Workspace
       uri = link_params[:url].scan(URI.regexp).blank? ? @message[:error] = 'invalid url' : URI(link_params[:url])
       # It validates the website is up and running
       if @message[:error].nil?
-        response = Net::HTTP.start(uri.host, uri.port, read_timeout: 300) { |http| http.request(uri) }
+        response = HTTParty.get(uri, timeout: 3)
         if response.respond_to?(:error) || response.code != 200
           @message[:error] = 'site is currently unavailable'
         else
