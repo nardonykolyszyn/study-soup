0f8f05b4de6f1081e47e05c89cd408b25c3aeee7
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-06-18 01:20:26 -0500

    migrate to pg

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@
 #
 # Table name: users
 #
-#  id                     :integer          not null, primary key
+#  id                     :bigint           not null, primary key
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  first_name             :string
