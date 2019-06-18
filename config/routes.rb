8e8601ce63c65051db7acfd41d0214e3a1060748
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-06-17 23:49:40 -0500

    include devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   
   root 'workspace#index'
 
-  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_up: 'signup', sign_out: 'logout' }
+  devise_for :users, path: 'auth', path_names: {
+    sign_in: 'login', sign_up: 'signup', sign_out: 'logout'
+  }, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 end
