9461887433d4cb76023c30b8a1440ed5a0a3a5b9
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-06-17 23:45:08 -0500

    add root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  
+  root 'workspace#index'
+
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_up: 'signup', sign_out: 'logout' }
 end
