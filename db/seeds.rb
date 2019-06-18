acfc1b9bf8f6c3272f922bfc11b1445cd413e126
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-06-18 11:57:07 -0500

    add links to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,12 @@ user.save!
 tags = %i[football programming couples instagram food friday dance travel sunshine newyork socialnetwork]
 
 100.times do
-  link = Link.new(url: Faker::Internet.unique.url).save
+  link = Link.create(url: Faker::Internet.unique.url)
   link.tags.build(name: tags.sample)
   link.save!
+end
+
+
+Link.all.each do |link|
+  user.links << link
 end
